da5aba44142fe2703667d13c67b9d8a8003f9593
geethikagrace <geethika2219@gmail.com>
2023-05-02 23:16:39 -0700

    dns

--- server.tf
+++ server.tf
@@ -69,7 +69,7 @@ resource "aws_instance" "instance" {
  resource "aws_route53_record" "records" {
   for_each = var.components
   zone_id = "Z08612761ONC1YALZWU0L"
-  name    = "${each.value["name"]} -dev.sonydevops.online"
+  name    = "${each.value["name"]}-dev.sonydevops.online"
   type    = "A"
   ttl     = 30
   records = [aws_instance.instance[each.value["name"]].private_ip]
