1f0b7e619ed9749060085b993c62cc067a642cca
geethikagrace <geethika2219@gmail.com>
2023-05-05 00:45:39 -0700

    env test

--- module/main.tf
+++ module/main.tf
@@ -31,7 +31,7 @@ resource "null_resource" "provisioner" {
 
 
 resource "aws_route53_record" "records" {
-  zone_id = "Z03986262CQPCHNJNZM9L"
+  zone_id = "Z08612761ONC1YALZWU0L"
   name    = "${var.component_name}-dev.rdevopsb72.online"
   type    = "A"
   ttl     = 30
