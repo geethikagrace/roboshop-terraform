8beeee609739c9f4966658e4c9c03a5beef78744
geethikagrace <geethika2219@gmail.com>
2023-05-13 14:14:43 -0700

    lookup

--- module/locals.tf
+++ module/locals.tf
@@ -3,11 +3,11 @@ locals {
   db_commands=[
   "rm -rf roboshop-shell",
   "git clone https://github.com/geethikagrace/roboshop-shell",
-"cd roboshop-shell",
-"sudo bash ${var.component_name}.sh ${var.password}"
+  "cd roboshop-shell",
+  "sudo bash ${var.component_name}.sh ${var.password}"
 ]
   app_commands=[
-    "sudo labauto ansible"
+    "sudo labauto ansible",
     "ansible-pull -i localhost, -U https://github.com/geethikagrace/roboshop-ansible roboshop.yml -e env=${var.env} -e role_name=${var.component_name}"
   ]
 }
