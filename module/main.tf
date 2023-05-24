9d75b4949786d96e5e2812392f94af0e90232be9
geethikagrace <geethika2219@gmail.com>
2023-05-24 03:20:08 -0700

    provisioner

--- module/main.tf
+++ module/main.tf
@@ -36,59 +36,59 @@ resource "aws_route53_record" "records" {
   records = [aws_instance.instance.private_ip]
 }
 
-resource "aws_iam_role" "role" {
-  name = "${var.component_name}-${var.env}-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Sid    = ""
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-      },
-    ]
-  })
-
-  tags = {
-    tag-key = "${var.component_name}-${var.env}-role"
-  }
-}
-
-
-
-resource "aws_iam_instance_profile" "instance_profile" {
-  name = "${var.component_name}-${var.env}-role"
-  role = aws_iam_role.role.name
-}
-
-
-
-resource "aws_iam_role_policy" "ssm-ps-policy" {
-  name = "${var.component_name}-${var.env}-ssm-ps-policy"
-  role = aws_iam_role.role.id
-
-  policy = jsonencode({
-    "Version": "2012-10-17",
-    "Statement": [
-      {
-        "Sid": "VisualEditor0",
-        "Effect": "Allow",
-        "Action": [
-          "kms:Encrypt",
-          "ssm:GetParameterHistory",
-          "ssm:GetParametersByPath",
-          "ssm:GetParameters",
-          "ssm:GetParameter"
-        ],
-        "Resource": [
-          "arn:aws:kms:us-east-1:838594267872:key/b24b0646-42c1-422c-ba99-bb9db96b3637",
-          "arn:aws:ssm:us-east-1:838594267872:parameter/dev.frontend.*"
-        ]
-      }
-    ]
-  })
-}
+#resource "aws_iam_role" "role" {
+#  name = "${var.component_name}-${var.env}-role"
+#
+#  assume_role_policy = jsonencode({
+#    Version = "2012-10-17"
+#    Statement = [
+#      {
+#        Action = "sts:AssumeRole"
+#        Effect = "Allow"
+#        Sid    = ""
+#        Principal = {
+#          Service = "ec2.amazonaws.com"
+#        }
+#      },
+#    ]
+#  })
+#
+#  tags = {
+#    tag-key = "${var.component_name}-${var.env}-role"
+#  }
+#}
+#
+#
+#
+#resource "aws_iam_instance_profile" "instance_profile" {
+#  name = "${var.component_name}-${var.env}-role"
+#  role = aws_iam_role.role.name
+#}
+#
+#
+#
+#resource "aws_iam_role_policy" "ssm-ps-policy" {
+#  name = "${var.component_name}-${var.env}-ssm-ps-policy"
+#  role = aws_iam_role.role.id
+#
+#  policy = jsonencode({
+#    "Version": "2012-10-17",
+#    "Statement": [
+#      {
+#        "Sid": "VisualEditor0",
+#        "Effect": "Allow",
+#        "Action": [
+#          "kms:Encrypt",
+#          "ssm:GetParameterHistory",
+#          "ssm:GetParametersByPath",
+#          "ssm:GetParameters",
+#          "ssm:GetParameter"
+#        ],
+#        "Resource": [
+#          "arn:aws:kms:us-east-1:838594267872:key/b24b0646-42c1-422c-ba99-bb9db96b3637",
+#          "arn:aws:ssm:us-east-1:838594267872:parameter/dev.frontend.*"
+#        ]
+#      }
+#    ]
+#  })
+#}
