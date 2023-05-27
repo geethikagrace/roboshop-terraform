b177884caf9b418ec86adbb17f2a6389e3817a54
geethika <geethilka2219@gmail.com>
2023-05-27 00:15:33 -0700

    role

--- module/main.tf
+++ module/main.tf
@@ -74,7 +74,7 @@ resource "aws_iam_role_policy" "ssm-ps-policy" {
   name = "${var.component_name}-${var.env}-ssm-ps-policy"
   role = aws_iam_role.role.id
 
-  policy = jsondecode({
+  policy = jsonencode({
     "Version" : "2012-10-17",
     "Statement" : [
       {
