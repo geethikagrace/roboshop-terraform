297e9dbb97f465bedd840430e8e9d19e8dd87bb2
geethika <geethilka2219@gmail.com>
2023-06-08 08:44:54 -0700

    vpc module

--- tags.auto.tfvars
+++
@@ -1,7 +0,0 @@
-tags = {
-  business_unit = "ecommerce"
-  business_type = "retail"
-  project ="roboshop"
-  cost_cent = 100
-  env = var.env
-}
---
+++ tags.tf
@@ -0,0 +1,9 @@
+locals {
+  tags = {
+    business_unit = "ecommerce"
+    business_type = "retail"
+    project ="roboshop"
+    cost_cent = 100
+    env = var.env
+  }
+}
