352b94f5b5541bbcde8a9b883e70286dfff4a4bb
geethika <geethilka2219@gmail.com>
2023-06-10 10:56:00 -0700

    vpc module

--- main.tf
+++ main.tf
@@ -10,7 +10,7 @@ module "vpc" {
 }
 
 module "web"{
-   source = "https://github.com/geethikagrace/tf-module-app.git"
+   source = "git::https://github.com/geethikagrace/tf-module-app.git"
 
    for_each = var.app
    instance_type =each.value["instance_type"]
