eb80ceb12885d63a1c71ff19ca6c029f3a678f21
geethika <geethilka2219@gmail.com>
2023-06-11 04:59:20 -0700

    web subnets

--- main.tf
+++ main.tf
@@ -1,20 +1,20 @@
 module "vpc" {
    source = "git::https://github.com/geethikagrace/tf-module-vpc.git"
 
-   for_each = var.vpc
-   cidr_block = each.value["cidr_block"]
-   subnets =each.value["subnets"]
-   tags = local.tags
-   env = var.env
+   for_each     = var.vpc
+   cidr_block   = each.value["cidr_block"]
+   subnets      = each.value["subnets"]
+   tags         = local.tags
+   env          = var.env
 }
 
-#
-#module "web"{
-#    source = "git::https://github.com/geethikagrace/tf-module-app.git"
-#
-#   for_each = var.app
-#   instance_type = each.value["instance_type"]
-#   subnet_id = element(lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null), 0)
-#}
+
+module "web"{
+    source = "git::https://github.com/geethikagrace/tf-module-app.git"
+
+   for_each      = var.app
+   instance_type = each.value["instance_type"]
+   subnet_id     = element(lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null), 0)
+}
 
 #subnets        = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null)
