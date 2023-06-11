186da802e87b6e3d7ad9397aba0ae7963866a8eb
geethika <geethilka2219@gmail.com>
2023-06-11 04:39:58 -0700

    web subnets

--- main.tf
+++ main.tf
@@ -6,15 +6,15 @@ module "vpc" {
    subnets =each.value["subnets"]
    tags = local.tags
    env = var.env
-
 }
 
-module "web"{
-    source = "git::https://github.com/geethikagrace/tf-module-app.git"
-
-   for_each = var.app
-   instance_type = each.value["instance_type"]
-   subnet_id = element(lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null), 0)
-}
+#
+#module "web"{
+#    source = "git::https://github.com/geethikagrace/tf-module-app.git"
+#
+#   for_each = var.app
+#   instance_type = each.value["instance_type"]
+#   subnet_id = element(lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null), 0)
+#}
 
 #subnets        = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null)
