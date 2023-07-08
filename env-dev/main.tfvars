622f2caff72a10d9a016a45f8d3f24100957dbf3
geethika <geethilka2219@gmail.com>
2023-07-08 10:28:11 +0530

    docdb

--- env-dev/main.tfvars
+++ env-dev/main.tfvars
@@ -47,6 +47,7 @@ app = {
     listener_priority= 1
     lb_type          = "public"
     dns_name         = "dev"
+    parameters       = []
 
   }
   catalogue = {
@@ -60,6 +61,7 @@ app = {
     app_port         = 8080
     listener_priority= 1
     lb_type          = "private"
+    parameters       =["docdb"]
 
 
 
--- main.tf
+++ main.tf
@@ -128,6 +128,7 @@ depends_on = [module.vpc, module.docdb, module.rds, module.elasticache, module.r
     app_port          = each.value["app_port"]
     listener_priority = each.value["listener_priority"]
     dns_name          = each.value["name"] ==  "frontend" ? each.value["dns_name"] : "${each.value["name"]}-${var.env}"
+    parameters        = each.value[parameters]
 
     subnet_ids        = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null)
     vpc_id            = lookup(lookup(module.vpc, "main", null), "vpc_id", null)
